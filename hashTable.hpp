ae95a5c6121e9e2982334723faec7498633d608f
connortribbett <49297850+connortribbett@users.noreply.github.com>
2019-04-04 21:57:13 -0600

    fixed errors

--- hashTable.hpp
+++ hashTable.hpp
@@ -54,23 +54,24 @@ public:
 private:
 	float lowerLoadThreshold = .65f;
 	float upperLoadThreshold = .85f;
+	float loadFactor = 0.0;
 	int primes[50] = {
 		61, 71, 83, 97, 107, 113, 127, 137, 151, 163, 173, 181, 191, 199, 211, 223, 229, 239, 241,257,269,277,293,307
 		313,331,347,353,367,383,397,409,421,433,443,457,467,487,488,509,523,547,791,1009,1223,1553,1999,2273, 2543,3001
 	};
 	int numPrimes = 50;
+	int currPrime = -1; //-1 = BASE_SIZE, otherwise corresponding index to primes array
 
 	unsigned int tableSize = BASE_SIZE;
 	unsigned int currentNodeCount = 0;
 	hashNode** HashTable = nullptr;
-	float loadFactor = 0.0;
 
 	unsigned int hash(std::string key);
 	int quadraticProbe(int startIndex);
 	void checkLoad();
 	void resize(bool factor);
 	void deleteTable();
-	void deleteLL();
+	void deleteLL(llNode* head);
 	void setupTable();
 };
 
